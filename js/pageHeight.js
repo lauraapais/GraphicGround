f6565a8ba5e9b40830d8ed8bd438e46d098c5d3a
Laura Pais <pais.laura@gmail.com>
2023-03-02 16:36:38 +0000

    mobile

--- js/pageHeight.js
+++ js/pageHeight.js
@@ -1,5 +1,7 @@
-window.addEventListener('resize', () => {
-    // We execute the same script as before
+function updateVh() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+}
+
+updateVh();
+window.addEventListener('resize', updateVh);
