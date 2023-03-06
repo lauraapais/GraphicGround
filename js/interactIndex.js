67979c7bcb3c305723fd9b1c2d1a614cf5b9dda2
Laura Pais <pais.laura@gmail.com>
2023-03-06 17:20:39 +0000

    about

--- js/interactIndex.js
+++ js/interactIndex.js
@@ -3,17 +3,9 @@ var menuMobileIcon = document.getElementById("openMenu");
 var closeMobileIcon = document.getElementById("closeMenu");
 var menuMobile = document.getElementById("menu");
 menuMobileIcon.addEventListener("click", function(){
-    if(menuMobile.style.top!== "-100vh") {
-        menuMobile.style.top = "-100vh";
-    } else {
-        menuMobile.style.top = "0";
-    }
+    menuMobile.style.left = "0";
 })
 
 closeMobileIcon.addEventListener("click", function(){
-    if(menuMobile.style.top!== "-100vh") {
-        menuMobile.style.top = "-100vh";
-    } else {
-        menuMobile.style.top = "0";
-    }
+    menuMobile.style.left = "100vw";
 })
